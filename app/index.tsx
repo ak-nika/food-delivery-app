053a9b8c213caf87b210279bd99057631d1ca80e
ak-nika <akingbayiojo@gmail.com>
2025-08-29 12:45:47 +0100

    expo and nativewind setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="mt-4 font-quicksand-light text-4xl text-primary">
+        Order your meal in just a few taps!
+      </Text>
+    </View>
+  );
+}
